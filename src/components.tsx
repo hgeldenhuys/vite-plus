904979e3459e60ebc440ac1325c8a5b9e7cc5c54
hgeldenhuys <hgeldenhuys@gmail.com>
2023-03-12 23:48:33 -0400

    Fixed: Added key to Loop's items

--- src/components.tsx
+++ src/components.tsx
@@ -22,7 +22,7 @@ export function Loop<T>({
   return (
     <>
       {items.map((item, i) => (
-        <Wrap Component={Component} item={item} index={i} />
+        <Wrap Component={Component} item={item} index={i} key={i} />
       ))}
     </>
   );
